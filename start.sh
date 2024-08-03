92f6692cbb2210b200f33b149666bf9e35187810
logan0116 <1809361150@qq.com>
2024-08-03 19:21:09 +0800

    update: env(dockerfile start)

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@
     --shm-size 64G \
     -v /home/mozinode4p/PycharmProjects/chat_server:/chat_server \
     -w /chat_server \
-    llm:v1.4.1 \
+    chat_server:v1.0 \
     python3 main.py --port 9010 --model_path model/qwen1_5-7b-chat-q4_0.gguf

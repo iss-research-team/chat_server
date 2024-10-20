71530d5b851443165a7f9a252382bca54bfb41a7
logan0116 <1809361150@qq.com>
2024-10-20 20:48:06 +0800

    update: deploy

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
 docker run \
-  -it \
+  -d \
   --rm \
   --network=host \
-  --name chat_server \
-  --gpus all \
-  --shm-size 64G \
-  -v /home/mozinode4p/PycharmProjects/chat_server:/chat_server \
+  --name chat_server_9010 \
+  --gpus "device=0" \
+  --shm-size 32G \
+  -v "$SCRIPT_DIR:/chat_server" \
   -w /chat_server \
   chat_server:v1.0 \
   python3 main.py --port 9010 --model_path model/qwen1_5-7b-chat-q4_0.gguf

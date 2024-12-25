35228ae73837e9a95d44a77550300a64ccfaff33
logan0116 <1809361150@qq.com>
2024-12-26 02:29:18 +0800

    update: deploy

--- start.sh
+++ start.sh
@@ -3,13 +3,11 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 docker run \
-  -d \
+  -it \
   --rm \
   --network=host \
   --name chat_server_9017 \
   --gpus "device=1" \
   --shm-size 32G \
-  -v "$SCRIPT_DIR:/chat_server" \
   -w /chat_server \
-  chat_server:v1.2 \
-  python3 main.py --port 9017 --model_path model/qwen2.5-14b-instruct-q8_0.gguf
+  chat_server:v1.3.1
